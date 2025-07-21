5589583e7e9f696db44d2a90f66bc0b3957a3e9f
aditiyadav01 <yadavaditi0501@gmail.com>
2025-07-21 19:32:20 +0530

    commit this change

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -2,6 +2,6 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `https://job-portal-app-kx45.onrender.com/api/v1/job`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
 export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
 export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
